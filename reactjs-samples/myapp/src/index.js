8cd526efa2b22c6c0ab8525327acd9b987ebee4d
Naveen Nagarle <learnfromnaveen@gmail.com>
2019-10-22 20:37:56 +0530

    react-first-program

---
+++ reactjs-samples/myapp/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const name  = 'Vivaan';
+
+const element = <h1>Hello, {name}!!!</h1>
+
+ReactDOM.render(element, document.getElementById('root') );
