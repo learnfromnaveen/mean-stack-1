4172580bf226828288726e1e5ba4616947e6fde5
Naveen Nagarle <learnfromnaveen@gmail.com>
2019-11-03 12:36:54 +0530

    react-fragment

--- reactjs-samples/myapp-routing/src/Users/Users.js
+++ reactjs-samples/myapp-routing/src/Users/Users.js
@@ -1,74 +1,84 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default class Users extends React.Component{ 
-    
-    constructor(props){ 
-        super(props);
-        this.state = { 
-            users: []
-        };
-    }
+export default class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
 
-    componentDidMount() { 
-        const url  =  'https://jsonplaceholder.typicode.com/users'; 
-        fetch(url)
-        .then(respose => respose.json())
-        .then(users => { 
-            this.setState( { 
-                users: users
-            });
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+      .then(respose => respose.json())
+      .then(users => {
+        this.setState({
+          users: users
         });
-    }
+      });
+  }
 
-     heroHeader() { 
-        return(
-            <thead> 
-                <tr>
-                    <th>Name</th>
-                    <th>Username</th>
-                    <th>Email</th>
-                    <th>Todos</th>
-                </tr>
-            </thead>
-        ) 
-    }
+  userHeader() {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Todos</th>
+        </tr>
+      </thead>
+    );
+  }
 
+  userColumns(user) {
+    return (
+      <>
+        <td>
+          <Link
+            to={{
+              pathname: "/user",
+              state: {
+                id: user.id
+              }
+            }}
+          >
+            {user.name}
+          </Link>
+        </td>
+        <td>{user.username}</td>
+        <td>{user.email}</td>
+        <td>
+          <Link
+            to={{
+              pathname: "/todos",
+              state: {
+                id: user.id
+              }
+            }}
+          >
+            View
+          </Link>
+        </td>
+      </>
+    );
+  }
 
-    heroBody() { 
-        const rows = this.state.users.map( ( user ) => ( 
-            <tr>
-                <td><Link to={{
-                   pathname: '/user',  
-                   state: { 
-                       id : user.id 
-                    } 
-                }}>{user.name}</Link></td>
-                <td>{user.username}</td>
-                <td>{user.email}</td>
-                <td>
-                <Link to={{
-                   pathname: '/todos',  
-                   state: { 
-                       id : user.id 
-                    } 
-                }}>View</Link>
-                </td>
-            </tr>
-        ));
-        return (
-            <tbody>  
-                { rows }
-            </tbody>
-        )
-    }
+  userBody() {
+    const rows = this.state.users.map(user => (
+      <tr>{this.userColumns(user)}</tr>
+    ));
+    return <tbody>{rows}</tbody>;
+  }
 
-    render() { 
-        return(
-            <table className='table table-striped'> 
-                {this.heroHeader()}
-                { this.heroBody()}
-            </table>
-        );
-    }
+  render() {
+    return (
+      <table className="table table-striped">
+        {this.userHeader()}
+        {this.userBody()}
+      </table>
+    );
+  }
 }
